1ef92a74b364b67f9e8ca909a08c6bf743b6bb2b
willsn <willsn@gmail.com>
2020-08-13 16:43:07 -0600

    still working on maze

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -61,111 +61,114 @@ player = Player(world.starting_room)
 
 #To solve this path, you'll want to construct your own traversal graph.
 
-## This is my "visited"
-traversal_graph = {}
 # this is the sub- data structure
 links_to_other_rooms = {}
+
+## This is my "visited"
+traversal_graph = {}
+
 
 # traversal_path = ['n', 'n']
 traversal_path = []
 
 q = Queue()
 
-q.enqueue([player.current_room.id])
+#print(world.starting_room.id)
+q.enqueue([world.starting_room.id])
+
+
+def get_neighbors(room):
+    return [0,1,2]
+    
+
+
 
 while q.size() > 0:
+
+    print('~~~~~NEXT ITERATION~~~~~~')
+    ## check current room
+    current_room = player.current_room.id
+    print(f'CURRENT ROOM: {current_room}')
 
     path = q.dequeue()
     most_recent_room = path[-1]
 
-    #Instead of searching for a target vertex, you are searching for an exit with a '?' as the value. 
-     # If an exit has been explored, you can put it in your BFS queue like normal.
-    #BFS will return the path as a list of room IDs. You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
-
-
     # what exits are avaliable in this current room?
     exit_list = player.current_room.get_exits()
 
+    for direction in exit_list:
+        print(f"---> DIRECTION: {direction}")
+        links_to_other_rooms[direction] = "?"  
 
+
+    #BFS will return the path as a list of room IDs. You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
+
+
+    print(traversal_graph)
+
+    # If an exit has been explored, you can put it in your BFS queue like normal.
     if most_recent_room not in traversal_graph:
-        if most_recent_room == "?": ## This will need to be fixed
-            ## update the links to other rooms
-            pass
+        #Instead of searching for a target vertex, you are searching for an exit with a '?' as the value. 
+        # if item in most_recent_room == "?": ## This will need to be fixed
+        #     ## update the links to other rooms
+        #     pass
     
         traversal_graph[most_recent_room] = exit_list
 
-        for direction in exit_list:
-            new_path = path.copy()
-            new_path.append(direction)
-            q.enqueue(new_path)
+        
 
-    print(new_path)
+        for next_room in get_neighbors(most_recent_room):
+            new_path = path.copy()
+            new_path.append(next_room)
+            q.enqueue(new_path)
+    
+    #     #picks a random unexplored direction from the player's current room,
+    random_direction = random.choice(player.current_room.get_exits())
+
+    #     #  travels and logs that direction
+    print(f'TRAVELING IN A "{random_direction.upper()}" DIRECTION')
+    player.travel(random_direction)
+
+    traversal_path = new_path
+    traversal_path.append(random_direction)
+    print(f'RUNNING TRAVERSAL PATH: {traversal_path}')
 
         
 
+print(f'FINAL TRAVERSAL PATH: {traversal_path}')
+print(f'FINAL TRAVERSAL GRAPH: {traversal_graph}')
 
 
 
 
+    # print(f'EXIT LIST: {exit_list}')    
+    # ## Populate the list with "?"
+    # for direction in exit_list:
+    #     print(f"---> DIRECTION: {direction}")
+    #     links_to_other_rooms[direction] = "?"  
 
-
-
-
-
-print("-------------------")
-
-
-# ## Keep grinding until I go through all the rooms
-# while len(traversal_graph) < len(room_graph):
-
-#     print('~~~~~NEXT ITERATION~~~~~~')
-#         ## check current room
-#     current_room = player.current_room.id
-#     print(f'CURRENT ROOM: {current_room}')
-
-#     # what exits are avaliable in this current room?
-#     exit_list = player.current_room.get_exits()
+     ## updates the traversal graph
+    # for item in traversal_graph:
+    #     print(traversal_path[item])
+    #     print(traversal_path[-1])
+    #     if traversal_graph[item] == traversal_path[-1]:
+    #         traversal_graph[item] = "THE ROOM PREVIOUS"
     
-#     #picks a random unexplored direction from the player's current room,
-#     random_direction = random.choice(player.current_room.get_exits())
+    # traversal_graph[player.current_room.id] = links_to_other_rooms
 
-#     print(f'EXIT LIST: {exit_list}')    
-#     ## Populate the list with "?"
-#     for direction in exit_list:
-#         print(f"---> DIRECTION: {direction}")
-#         links_to_other_rooms[direction] = "?"  
-
-#      ## updates the traversal graph
-#     # for item in traversal_graph:
-#     #     print(traversal_path[item])
-#     #     print(traversal_path[-1])
-#     #     if traversal_graph[item] == traversal_path[-1]:
-#     #         traversal_graph[item] = "THE ROOM PREVIOUS"
-    
-#     traversal_graph[player.current_room.id] = links_to_other_rooms
-
-#     #  travels and logs that direction
-#     print(f'TRAVELING IN A "{random_direction.upper()}" DIRECTION')
 
     
-
-#     player.travel(random_direction)
     
-#     traversal_path.append(random_direction)
-
-      
-#     print(f'RUNNING TRAVERSAL PATH: {traversal_path}')
-#     print(f'RUNNING TRAVERSAL GRAPH: {traversal_graph}') 
-
-    
-# # then loops
 
 
-# print(f'TRAVERSAL PATH: {traversal_path}')
-# print(f'TRAVERSAL GRAPH: {traversal_graph}')
 
-## Depth first traversal
-#When you reach a dead-end (i.e. a room with no unexplored paths), walk back to the nearest room that does contain an unexplored path.
+
+
+
+
+
+
+
 
 
 
